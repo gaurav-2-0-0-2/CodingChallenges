7e4e9fcf3c65c1479050c3c85945a0a28a7d5e4e
Gaurav Tak <gauravtak996@gmail.com>
2024-02-19 20:33:38 +0530

    ccwc step-6 pending

---
+++ ccwc/src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2{
+        eprintln!("Usage: {}", args[0]);
+        std::process::exit(1);
+    }
+    // Taking file name from the vector args
+    let filename = &args[2];
+    let file = File::open(filename)?;
+    let metadata = file.metadata()?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    //println!("{}", contents);
+    let mut count_lines = 0;
+    let mut word_count = 0;
+    let byte_size = metadata.len();
+    let char_count = contents.chars().count();
+
+    for _line in contents.lines() {
+        count_lines += 1;
+        word_count += _line.split_whitespace().count();
+    }
+
+    if args[1] == "-l" {
+        println!("{} {}", count_lines, filename);
+    } else if args[1] == "-c" {
+        println!("{} {}", byte_size, filename);
+    } else if args[1] == "-w" {
+        println!("{} {}", word_count, filename);
+    }else if args[1] == "-m"{
+        println!("{} {}", char_count, filename);
+    }else if args[1].to_lowercase() == filename.to_lowercase() {
+        println!("{} {} {} {} {}", count_lines, word_count, byte_size, char_count, filename);
+    }
+    else {
+        eprintln!("Wrong option: {}", args[1]);
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
