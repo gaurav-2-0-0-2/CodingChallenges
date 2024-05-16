4dbc9db2a483fef6246c2814678de82ec384efbd
Gaurav Tak <gauravtak996@gmail.com>
2024-05-16 22:17:24 +0530

    created a lexer function reading cli arguments

--- jpar/src/main.rs
+++ jpar/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use std::env;
+
+fn lex(input_string: &String){
+    let mut vec = Vec::new();
+    for c in input_string.chars(){
+        vec.push(c);
+    }
+    println!("{:?}", vec);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    lex(&contents);
 }
